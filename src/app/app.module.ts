e2c1e72ecb9331319c96c673201fd530f705467d
SumayyaGitHub <jsumayya@gmail.com>
2021-10-27 12:53:50 +0530

    4 files modified, 5 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { DndDirective } from './dnd.directive';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, DndDirective, ProgressComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
